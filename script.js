ece584b437d655a2617187661e2f2f6540729a0e
eduardovieiraraimundo-ctrl <eduardo.vieira.raimundo@escola.pr.gov.br>
2026-03-24 16:19:53 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,57 +1,11 @@
-const display = document.getElementById('resultado');
-const secaoEstados = document.getElementById('estados');
-const secaoCidades = document.getElementById('cidades');
-const listaEstados = document.getElementById('lista-estados');
-const listaCidades = document.getElementById('lista-cidades');
+const avanca = document.querySelectorAll('.btn-proximo');
 
-const dadosSudeste = {
-    "Espírito Santo": ["Vitória", "Vila Velha"],
-    "Minas Gerais": ["Belo Horizonte", "Ouro Preto"],
-    "Rio de Janeiro": ["Rio de Janeiro", "Búzios"],
-    "São Paulo": ["Santos", "Aparecida"]
-};
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo')
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo')
 
-
-document.querySelectorAll('.btn-regiao').forEach(botao => {
-    botao.addEventListener('click', () => {
-        const regiao = botao.textContent.toLowerCase();
-        
-      
-        if (regiao === "sudeste") {
-            mostrarEstados();
-        } else {
-            secaoEstados.style.display = "none";
-            secaoCidades.style.display = "none";
-            display.textContent = "Em breve teremos opções para " + regiao + "!";
-        }
-    });
-});
-
-function mostrarEstados() {
-    listaEstados.innerHTML = ""; 
-    secaoEstados.style.display = "block";
-    secaoCidades.style.display = "none";
-    display.textContent = "";
-
-    Object.keys(dadosSudeste).forEach(estado => {
-        const btn = document.createElement("button");
-        btn.textContent = estado;
-        btn.onclick = () => mostrarCidades(estado);
-        listaEstados.appendChild(btn);
-    });
-}
-
-function mostrarCidades(estado) {
-    listaCidades.innerHTML = ""; // Limpa botões anteriores
-    secaoCidades.style.display = "block";
-    
-    dadosSudeste[estado].forEach(cidade => {
-        const btn = document.createElement("button");
-        btn.textContent = cidade;
-        btn.onclick = () => {
-            display.textContent = `Boa escolha! Vamos para ${cidade} em ${estado}!`;
-            display.style.color = "#0b132b"; // Cor corrigida do seu CSS
-        };
-        listaCidades.appendChild(btn);
-    });
-}
+        atual.classList.remove('ativo')
+        document.getElementById(proximoPasso).classList.add('ativo')
+    })
+})
